583ff8a8326b72dddce7045b595cf8a068429097
yurkohudyma <hudyma@gmail.com>
2025-11-04 09:04:26 +0200

    init commit

---
+++ src/main/java/ua/hudyma/NovaPoshta2025App.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaPoshta2025App {
+    public static void main(String[] args) {
+        SpringApplication.run(NovaPoshta2025App.class, args);
+    }
+}
